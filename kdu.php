10bf28032cdfd6e2b953bfee17825518f81a901a
Khoo Kean Feng <khozai0615@gmail.com>
2019-07-16 21:47:53 +0800

    first

---
+++ kdu.php
@@ -0,0 +1,262 @@
+<?php
+echo "<style>
+body, html {
+  height: 100%;
+  margin: 0;
+}
+
+.bg {
+  /* The image used */
+  background-image: url(\"kduimages/kdu.png\");
+
+  /* Full height */
+  height: 50%; 
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 1350px 340px ;
+  
+  /* Needed to position the navbar */
+  position: relative;
+}
+
+/* Position the navbar container inside the image */
+.container {
+  position: absolute;
+  margin: 20px;
+  width: auto;
+}
+
+img {
+  border-radius: 50%; 
+  border-style: solid;
+  border-color: black;
+  margin: 160 20px;
+  
+}
+
+body {
+  margin: 0;
+  font-family: Arial;
+}
+</style>";
+echo"<div class=\"bg\"><img src=\"kduimages/kdulogo.png\" height='350px' width='350px'>";
+echo"<center><h1>Programmes Offered</h1></center>";
+echo"<div class=\"range\">
+         
+        
+         <div class=\"cell-md-12\">
+         
+         <div class=\"list-order\">
+         	<div class=\"shell\">
+<div class=\"range range-sm-center\">
+<div class=\"cell-lg-10 offset-top-40\">
+<div id=\"tabs-1\" class=\"tabs-custom tabs-horizontal tabs-corporate\">
+<div class=\"tab-content text-secondary\">
+<div id=\"tabs-1-2\" class=\"tab-pane fade\">
+<div>&nbsp;
+<table border=\"0\" align=\"center\">
+<tbody>
+<tr>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>
+<div align=\"center\" class=\"blackLink\"><a href=\"#A\">A</a> | <a href=\"#B\">B</a> | <a href=\"#C\">C</a> | <a href=\"#D\">D</a> | <a href=\"#E\">E</a> | <a href=\"#F\">F</a> | <a href=\"#G\">G</a> | <a href=\"#H\">H</a> | <a href=\"#I\">I</a> | <a href=\"#J\">J</a> | <a href=\"#K\">K</a> | <a href=\"#L\">L</a> | <a href=\"#M\">M</a> | <a href=\"#N\">N</a> | <a href=\"#O\">O</a> | <a href=\"#P\">P</a> | <a href=\"#Q\">Q</a> | <a href=\"#R\">R</a> | <a href=\"#S\">S</a> | <a href=\"#T\">T</a> | <a href=\"#U\">U</a> | <a href=\"#V\">V</a> | <a href=\"#W\">W</a> | <a href=\"#X\">X</a> | <a href=\"#Y\">Y</a> | <a href=\"#Z\">Z</a></div>
+</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+</tr>
+</tbody>
+</table>
+</div>";
+echo"<div align=\"center\">
+<table width=\"100%\" border=\"0\" cellpadding=\"1\" cellspacing=\"1\">
+<tbody>
+<tr>
+<td align=\"left\"><a name=\"A\" id=\"A\"></a><strong>A</strong><hr /></td>
+</tr>
+<tr>
+<td align=\"left\">
+<ul>
+<li>Accounting</li>
+<li>Arts</li>
+</ul>
+</td>
+</tr>
+</tbody>
+</table>";
+echo"<table width=\"100%\" border=\"0\" cellpadding=\"1\" cellspacing=\"1\">
+<tbody>
+<tr>
+<td align=\"left\"><a name=\"B\" id=\"B\"></a><strong>B</strong><hr /></td>
+</tr>
+<tr>
+<td align=\"left\">
+<ul>
+<li>Business</li>
+<li>Bachelor of Acoountancy</li>
+<li>Business Management</li>
+<li>Bachelor of Computer Science (Hons)</li>
+<li>Bachelor of Computer Science (Hons) in Computer and Network Technology</li>
+<li>Bachelor of Information Systems (Hons)</li>
+<li>Bachelor of Arts (Hons) in International Hotel & Tourism Management</li>
+<li>Bachelor of Arts (Hons) In International Culinary Arts</li>
+<li>Bachelor of Science (Hons) Electrical & Electronic Engineering</li>
+<li>Bachelor of Science (Hons) Mechatronics Engineering</li>
+</ul>
+</td>
+</tr>
+</tbody>
+</table>";
+echo"<table width=\"100%\" border=\"0\" cellpadding=\"1\" cellspacing=\"1\">
+<tbody>
+<tr>
+<td align=\"left\"><a name=\"C\" id=\"C\"></a><strong>C</strong><hr /></td>
+</tr>
+<tr>
+<td align=\"left\">
+<ul>
+<li>Cambridge A-Level</li>
+<li>Chinese Proficiency Test</li>
+<li>Computer Science</li>
+<li>Computer Studies</li>
+<li>Communication and Public Relations (Bachelor of Arts (Hons))</li>
+</ul>
+</td>
+</tr>
+</tbody>
+</table>";
+echo"<table width=\"100%\" border=\"0\" cellpadding=\"1\" cellspacing=\"1\">
+<tbody>
+<tr>
+<td align=\"left\"><a name=\"D\" id=\"D\"></a><strong>D</strong><hr /></td>
+</tr>
+<tr>
+<td align=\"left\">
+<ul>
+<li>Doctor of Philosophy(Business)</li>
+<li>Doctor of Philosophy(Engineering)</li>
+<li>Doctor of Philosophy(Hospitality and Tourism)</li>
+<li>Doctor of Philosophy(Computer Science)</li>
+<li>Digital Animation</li>
+</ul>
+</td>
+</tr>
+</tbody>
+</table>";
+echo"<table width=\"100%\" border=\"0\" cellpadding=\"1\" cellspacing=\"1\">
+<tbody>
+<tr>
+<td align=\"left\"><a name=\"E\" id=\"E\"></a><strong>E</strong><hr /></td>
+</tr>
+<tr>
+<td align=\"left\">
+<ul>
+<li>Electrical and Electronic Engineering (Mechatronics)</li>
+<li>Electrical and Electronic Engineering (Microelectronics)</li>
+</ul>
+</td>
+</tr>
+</tbody>
+</table>";
+echo"<table width=\"100%\" border=\"0\" cellpadding=\"1\" cellspacing=\"1\">
+<tbody>
+<tr>
+<td align=\"left\"><a name=\"H\" id=\"H\"></a><strong>H</strong><hr /></td>
+</tr>
+<tr>
+<td align=\"left\">
+<ul>
+<li>Hotel Management</li>
+</ul>
+</td>
+</tr>
+</tbody>
+</table>";
+echo"<table width=\"100%\" border=\"0\" cellpadding=\"1\" cellspacing=\"1\">
+<tbody>
+<tr>
+<td align=\"left\"><a name=\"I\" id=\"I\"></a><strong>I</strong><hr /></td>
+</tr>
+<tr>
+<td align=\"left\">
+<ul>
+<li>Interior Design</li>
+<li>Interior Architecture (BA(Hons))</li>
+<li>Intensive English Programme</li>
+<li>International Hotel Management</li>
+</ul>
+</td>
+</tr>
+</tbody>
+</table>";
+echo"<table width=\"100%\" border=\"0\" cellpadding=\"1\" cellspacing=\"1\">
+<tbody>
+<tr>
+<td align=\"left\"><a name=\"M\" id=\"M\"></a><strong>M</strong><hr /></td>
+</tr>
+<tr>
+<td align=\"left\">
+<ul>
+<li>Master of Business</li>
+<li>Master of Business Administration</li>
+<li>Master of Science in Hospitality and Tourism</li>
+<li>Mass Communication</li>
+<li>Media Production (Bachelor of Arts (Hons))</li>
+</ul>
+</td>
+</tr>
+</tbody>
+</table>";
+echo"<table width=\"100%\" border=\"0\" cellpadding=\"1\" cellspacing=\"1\">
+<tbody>
+<tr>
+<td align=\"left\"><a name=\"N\" id=\"N\"></a><strong>N</strong><hr /></td>
+</tr>
+<tr>
+<td align=\"left\">
+<ul>
+<li>Nursing</li>
+<li>New Media and Advertising (Bachelor of Arts (Hons))</li>
+</ul>
+</td>
+</tr>
+</tbody>
+</table>";
+echo"<table width=\"100%\" border=\"0\" cellpadding=\"1\" cellspacing=\"1\">
+<tbody>
+<tr>
+<td align=\"left\"><a name=\"P\" id=\"P\"></a><strong>P</strong><hr /></td>
+</tr>
+<tr>
+<td align=\"left\">
+<ul>
+<li>Professional Development Programmes</li>
+<li>Professional Chef Training</li>
+</ul>
+</td>
+</tr>
+</tbody>
+</table>";
+echo"<table width=\"100%\" border=\"0\" cellpadding=\"1\" cellspacing=\"1\">
+<tbody>
+<tr>
+<td align=\"left\"><a name=\"S\" id=\"S\"></a><strong>S</strong><hr /></td>
+</tr>
+<tr>
+<td align=\"left\">
+<ul>
+<li>Science</li>
+<li>Science (Engineering)</li>
+<li>Science in HOspotality and Tourism</li>
+</ul>
+</td>
+</tr>
+</tbody>
+</table>";
+
+
+?>
